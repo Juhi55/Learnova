9085d35fbd8501979437f9c1fd640c88222a3819
juhi55 <cjuhi888@gmail.com>
2026-07-05 20:18:17 +0530

    Fix  deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://learnova-backend-9yuu.onrender.com",
+  baseURL: "https://learnova-backend-9yuu.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
